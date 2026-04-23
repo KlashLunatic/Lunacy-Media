f2b957e575bcade8240153fd89f9d3982631971e
KlashLunatic <253122830+KlashLunatic@users.noreply.github.com>
2026-04-22 22:01:11 -0400

    Fix logo: use CDN URL

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -9,7 +9,7 @@ interface LogoProps {
 const Logo: FC<LogoProps> = ({ width = 48, height = 64, className = '' }) => {
   return (
     <img
-      src="/manus-storage/Logo_7bef1327.svg"
+      src="https://files.manuscdn.com/user_upload_by_module/session_file/310419663032200270/lVeZNtlDodoCBAGY.svg"
       alt="Lunacy Media Logo"
       width={width}
       height={height}
